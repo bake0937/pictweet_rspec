5859253f7450f312965d20b9ba8fb398d326938d
bake0937 <sadayasu.info@gmail.com>
2017-03-15 22:48:00 +0900

    Fixed the expression of "it" to Japanese

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 describe User do
-  describe '#ユーザー作成' do
-    it "is invalid without a nickname" do
+  describe '#ユーザー登録' do
+
+    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nicknameが空では登録できない" do
       user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
-    it "is invalid without a email" do
+    it "emailが空では登録できない" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
