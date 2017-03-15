d5760d6aa89a0d3cb86e093177eaae3aa213079a
bake0937 <sadayasu.info@gmail.com>
2017-03-15 18:22:39 +0900

    Fix description using factory_girl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,23 +2,13 @@ require 'rails_helper'
 describe User do
   describe '#ユーザー作成' do
     it "is invalid without a nickname" do
-      user = User.new(
-        nickname: "",
-        email: "aaaa@sample.com",
-        password: "12345678",
-        password_confirmation: "12345678"
-      )
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
     it "is invalid without a email" do
-      user = User.new(
-        nickname: "Michael",
-        email: "",
-        password: "12345678",
-        password_confirmation: "12345678"
-      )
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
