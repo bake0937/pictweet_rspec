2722ae04d37f295fdb559de73aef2f7b67006d81
bake0937 <sadayasu.info@gmail.com>
2017-03-16 23:21:21 +0900

    Add "renders the :new template"

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe TweetsController do
 
+  describe 'GET #new' do
+    it "newアクションをリクエストした後、new.html.erbに遷移するか" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
 end
